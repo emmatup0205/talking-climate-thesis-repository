f972bff9f61a2f926a0d17fc28a6f2430b2ec17e
emmatup0205 <emmatupone@gmail.com>
2026-05-14 21:08:36 -0400

    Create thesis_analysis.R

---
+++ code/thesis_analysis.R
@@ -0,0 +1,5 @@
+# Thesis analysis script
+# Emma Valentina Tupone
+# Climate conversations, anxiety, optimism, and pro-environmental behavior
+
+# Note: Data are not publicly available due to IRB restrictions
